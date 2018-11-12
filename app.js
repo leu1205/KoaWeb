e3c8eeafa34ab1ef20b3bdfe21faebe06c924819
leu1205 <leu1205@gmail.com>
2018-11-12 21:54:44 +0800

    npm init

---
+++ app.js
@@ -0,0 +1,29 @@
+const koa = require('koa');
+const logger = require('koa-logger');
+const bodyParser = require('koa-bodyparser');
+const views = require('koa-views');
+const static = require('koa-static');
+const session = require('koa-session');
+const validStatus = require('./validStatus');
+const router = require('./route');
+const app = new koa();
+/*const config = {
+    key: 'login',
+    maxAge: 86400000,
+    overwrite: true,
+    httpOnly: true,
+    secure: true,
+    signed: false
+}*/
+//router.use(session(app));
+
+app.use(bodyParser());
+app.use(views(__dirname + "/view", {extension: 'pug'}));
+app.use(static(__dirname, {extensions: ['jpg','css']}));
+//app.use(validStatus);
+app.use(router.routes());
+app.use(logger());
+
+
+
+app.listen(3000);
